7d7d4d140911244e2c8498ccc145d7c426d45fe0
Kainy Onodera <kainykawai@gmail.com>
2024-06-18 16:38:01 +0300

    fixednav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar =  () => {
   const user =  getUser();
 
   return (
-    <nav className="sticky  h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+    <nav className="  h-14 inset-x-0 top-0 z-30 w-full border-b sticky border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex container h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
