ede77944a2ce6a0b9f67feb3c87faa38bda26396
kainy01 <kainykawai@gmail.com>
2024-06-21 17:05:24 +0300

    Feat : added input to swtch between pages

--- src/components/PdfView.tsx
+++ src/components/PdfView.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
 import { useResizeDetector } from "react-resize-detector";
@@ -9,6 +8,10 @@ import { useToast } from "./ui/use-toast";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "@/lib/utils";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 type PdfViewProps = {
@@ -19,7 +22,29 @@ const PdfView = ({ url }: PdfViewProps) => {
   const [numPage, setNumPage] = useState<number>();
   const [curPage, setCurPage] = useState<number>(1);
   const { toast } = useToast();
-  const { width = 0, ref } = useResizeDetector();
+
+  const PageNumberSchema = z.object({
+    page: z
+      .string()
+      .refine((num) => Number(num) > 0 && Number(num) <= numPage!),
+  });
+  type TCPageNumberSchema = z.infer<typeof PageNumberSchema>;
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<TCPageNumberSchema>({
+    defaultValues: {
+      page: "1",
+    },
+    resolver: zodResolver(PageNumberSchema),
+  });
+  const { width, ref } = useResizeDetector();
+  const handlePageSubmit = handleSubmit(({ page } : TCPageNumberSchema) => {
+    setCurPage(Number(page));
+    setValue("page", String(page));
+  });
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
@@ -35,7 +60,15 @@ const PdfView = ({ url }: PdfViewProps) => {
             <ChevronDown className="h-4 w-4" />
           </Button>
           <div className="flex items-center gap-1.5">
-            <Input className="w-12 h-8" />
+            <Input
+              {...register("page")}
+              className={cn("w-12 h-8", errors.page && " focus-visible:ring-red-500")}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+               handlePageSubmit();
+                }
+              }}
+            />
             <p className=" text-zinc-700 text-sm space-x-1">
               <span>/</span>
               <span>{numPage ?? "x"}</span>
