b8987761e60add44cccc348602e0f89acd0714df
kainy01 <kainykawai@gmail.com>
2024-06-25 16:20:28 +0300

    added strip code

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,10 +1,13 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { z } from "zod";
 import { db } from "@/db";
-//END POINTS
+
 import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
+import { absoluteUrl } from "@/lib/utils";
+import { getUserSubscriptionPlan, stripe } from "@/lib/stripe";
+import { PLANS } from "@/config/stripe";
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
@@ -89,23 +92,23 @@ export const appRouter = router({
           createdAt: "desc",
         },
         cursor: cursor ? { id: cursor } : undefined,
-        select:{
-          id:true,
-          createdAt:true,
-          isUserMessage:true,
-          text:true,
-        }
-      })
+        select: {
+          id: true,
+          createdAt: true,
+          isUserMessage: true,
+          text: true,
+        },
+      });
 
-      let nextCursor : typeof cursor | undefined = undefined;
+      let nextCursor: typeof cursor | undefined = undefined;
       if (messages.length > limit) {
-       const nextItem = messages.pop()
-        nextCursor = nextItem?.id
+        const nextItem = messages.pop();
+        nextCursor = nextItem?.id;
       }
       return {
         messages,
-        nextCursor
-      }
+        nextCursor,
+      };
     }),
 
   getFileUploadStatus: privateProcedure
@@ -139,6 +142,52 @@ export const appRouter = router({
 
       return file;
     }),
+
+  createStripeSession: privateProcedure.mutation(async ({ ctx }) => {
+    const { userId } = ctx;
+
+    const billingUrl = absoluteUrl("/dashboard/billing");
+
+    if (!userId) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!dbUser) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    const subscriptionPlan = await getUserSubscriptionPlan();
+
+    if (subscriptionPlan.isSubscribed && dbUser.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: dbUser.stripeCustomerId,
+        return_url: billingUrl,
+      });
+
+      return { url: stripeSession.url };
+    }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: billingUrl,
+      cancel_url: billingUrl,
+      payment_method_types: ["card", "paypal"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      line_items: [
+        {
+          price: PLANS.find((plan) => plan.name === "Pro")?.price.priceIds.test,
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId: userId,
+      },
+    });
+
+    return { url: stripeSession.url };
+  }),
 });
 
 export type AppRouter = typeof appRouter;
