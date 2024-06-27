951398cf2da381b2a3e80c824ec3d784b4e9ac89
kainy01 <kainykawai@gmail.com>
2024-06-27 11:55:46 +0300

    fixed get error on vercel

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,26 +5,20 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "@/app/_trpc/client";
 import { httpBatchLink } from "@trpc/client";
 const Providers = ({ children }: PropsWithChildren) => {
-
-  
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          // url: "http://localhost:3000/api/trpc",
+          url: "/api/trpc",
         }),
       ],
     })
   );
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider
-      client={queryClient}
-      >
-
-      {children}
-      </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </trpc.Provider>
   );
 };
