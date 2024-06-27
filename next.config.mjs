92530c9403ac03bfc8258b091e82f0afdf27e27d
kainy01 <kainykawai@gmail.com>
2024-06-27 11:42:21 +0300

    fixd config error

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
-    return [
-      {
-        source: '/sign-in',
-        destination: '/api/auth/login',
-        permanent: true,
+        return [
+          {
+            source: '/sign-in',
+            destination: '/api/auth/login',
+            permanent: true,
+          },
+          {
+            source: '/sign-up',
+            destination: '/api/auth/register',
+            permanent: true,
+          },
+        ]
       },
-      {
-        source: '/sign-up',
-        destination: '/api/auth/register',
-        permanent: true,
-      },
-    ]
-  },
-
   webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, webpack }
-  ) => {
-    config.resolve.alias.canvas = false
-    config.resolve.alias.encoding = false
-    return config
-  },
-}
+      config,
+      { buildId, dev, isServer, defaultLoaders, webpack }
+    ) => {
+      config.resolve.alias.canvas = false
+      config.resolve.alias.encoding = false
+      return config
+    },
+  }
 
-module.exports = nextConfig
+export default nextConfig;
